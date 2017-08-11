740f7ffdcf30f6ca8e9bd245eec85d18436fa61d
zakms <mz.soufiani@gmail.com>
2017-08-12 00:03:35 +1000

    add main logic with praw

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # import socket to display username
 import socket
+import json
+import urllib2
+import httplib
+import time
+import praw
 
 
 # bool to keep the program running
@@ -17,12 +22,15 @@ valid_commands = ['ls', 'cd', 'cd .', 'cd ..', 'cat']
 
 # Function to parse user inputs
 def input_parser(user_input):
-    if user_input in valid_commands:
+    user_input_split = user_input.split(' ')
+    if user_input_split[0] in valid_commands:
         print 'user entered valid command \n'
         print 'user just entered this:  ' + user_input
+        user_input_split = user_input.split(' ')
 
-        if (user_input == 'cd'):
-            cd_command()
+
+        if (user_input_split[0] == 'cd'):
+            cd_command(user_input_split[1])
 
         elif (user_input == 'cd .'):
             cd_dot_command()
@@ -40,8 +48,10 @@ def input_parser(user_input):
         print 'user entered invalid command'
 
 # various functions for every command
-def cd_command():
-    print 'cd command'
+def cd_command(subReddit):
+    print 'cd command \n'
+    print 'displaying top subs'
+    main_api_logic(subReddit)
 
 def cd_dot_command():
     print 'cd . command'
@@ -52,9 +62,34 @@ def cd_dot_dot_command():
 def ls_command():
     print 'ls command'
 
-
 def cat_command():
     print 'cat command'
+
+# function to print line
+def print_line():
+    print '--------------------------------------------------------------------------------------------------------\n'
+
+
+##################################################
+
+#####   LOGIC FOR API   ##########################
+
+##################################################
+
+def main_api_logic(subReddit):
+    # create an instance of reddit and subreddit using praw
+    # Zak's credentials lolz
+    reddit = praw.Reddit(client_id='ilgRMpq1J-Kx4w',
+                         client_secret='wsjtyoqyNVKm0Ds8nTTGJNU-YIE',
+                         user_agent='inter_webz')
+    subreddit = reddit.subreddit(subReddit)
+
+    for submission in subreddit.top(limit=10):
+        print_line()
+        print ('Title: ')
+        print(submission.title)  # Submission title
+        print ('Score: ')
+        print (submission.score)  # Submission Score
 
 
 # Constant loop to keep the program running
