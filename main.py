b1580d2bc0b65e24172242646afa93dde941810f
zakms <mz.soufiani@gmail.com>
2017-08-11 22:32:06 +1000

    add functions for every command

--- main.py
+++ main.py
@@ -1,12 +1,60 @@
 # import socket to display username
 import socket
 
+
+# bool to keep the program running
 programRunning = True
 
+# list of valid commands
+valid_commands = ['ls', 'cd', 'cd .', 'cd ..', 'cat']
+
+
+##################################################
+
+#####   NEED TO REFRACTOR FUNCTION IN FILES ######
+
+##################################################
 
 # Function to parse user inputs
 def input_parser(user_input):
-    print 'user just entered this:  ' + user_input
+    if user_input in valid_commands:
+        print 'user entered valid command \n'
+        print 'user just entered this:  ' + user_input
+
+        if (user_input == 'cd'):
+            cd_command()
+
+        elif (user_input == 'cd .'):
+            cd_dot_command()
+
+        elif (user_input == 'cd ..'):
+            cd_dot_dot_command()
+
+        elif (user_input == 'ls'):
+            ls_command()
+
+        elif (user_input == 'cat'):
+            cat_command()
+
+    else:
+        print 'user entered invalid command'
+
+# various functions for every command
+def cd_command():
+    print 'cd command'
+
+def cd_dot_command():
+    print 'cd . command'
+
+def cd_dot_dot_command():
+    print 'cd .. command'
+
+def ls_command():
+    print 'ls command'
+
+
+def cat_command():
+    print 'cat command'
 
 
 # Constant loop to keep the program running
