57e5632454b52832a2ef2e22626b9f17a1662b3b
Khuslen Baatarsukh <khuslen96@yahoo.com>
2017-08-12 18:57:01 +1000

    begin refactoring

---
+++ refactored/userInput.py
@@ -0,0 +1,30 @@
+import commandFuncs
+
+# list of valid commands
+valid_commands = ['ls', 'cd', 'cd .', 'cd ..', 'cat', 'next']
+
+# Function to parse user inputs
+def input_parser(user_input):
+    print 'using the iuserinput py version'
+    # split user_input to separate command and argument
+    user_input_split = user_input.split(' ')
+
+    top_level_command = user_input_split[0]
+
+    print 'user input split:' + user_input_split
+
+    if top_level_command in valid_commands:
+        if (top_level_command == 'cd'):
+            commandFuncs.cd_command(user_input_split)
+
+        elif (top_level_command == 'ls'):
+            commandFuncs.ls_command()
+
+        elif (top_level_command == 'cat'):
+            commandFuncs.cat_command()
+
+        elif (top_level_command == 'next'):
+            commandFuncs.next_command()
+
+    else:
+        print 'user entered invalid command'
---
+++ refactored/userSession.py
@@ -0,0 +1,16 @@
+class userSession:
+    def __init__(self, subreddit):
+        currentSubreddit = subreddit
+        currentSubmission = ''
+        currentComment = ''
+        currentPosition = subreddit # are we in a subreddit, submission or a comment?
+        nextNum = 0
+        
+    def changeSubreddit():
+        print "subreddit"
+
+    def changeSubmission():
+        print "submission"
+
+    def changeComment():
+        print "comment"
