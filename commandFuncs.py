b7d53bfcf6025dea84aa963d58a6af5fb8515e13
zakms <mz.soufiani@gmail.com>
2017-08-13 14:23:16 +1000

    final delet

---
+++ commandFuncs.py
@@ -0,0 +1,136 @@
+import userSession
+import redditGet
+import redditPost
+import helpInfo
+
+default_sort = 'hot'
+valid_sorts = ['hot', 'new', 'rising', 'controversial', 'top']
+
+def cd_command(user_input_split):
+
+    if user_input_split[1] == '.':
+        cd_dot_command()
+
+    elif user_input_split[1] == '..':
+        cd_dot_dot_command()
+
+    elif user_input_split[1][:3] == '../':
+        get_dots = user_input_split[1].split('/')
+        for x in range(0, len(get_dots)):
+            cd_dot_dot_command()
+
+    elif user_input_split[1].isdigit():
+        if userSession.currentSession.currentState == 'subreddit':
+            userSession.currentSession.currentThread = int(user_input_split[1])
+            userSession.currentSession.currentState = 'thread'
+
+            userSession.currentSession.nextNumComment = 0
+        
+        elif userSession.currentSession.currentState == 'thread':
+            userSession.currentSession.currentSubmission = int(user_input_split[1])
+            userSession.currentSession.currentState = 'submission'
+            redditGet.readPost(int(user_input_split[1]))
+            # userSession.currentSession.nextNum = 0
+            userSession.currentSession.nextNumComment = 0
+
+        elif userSession.currentSession.currentState == 'submission':
+            userSession.currentSession.currentComment = int(user_input_split[1])
+            userSession.currentSession.currentState = 'comment'
+        
+        elif userSession.currentSession.currentState == 'comment':
+            # TODO:
+            print "hi"
+
+    elif userSession.currentSession.currentState == 'attheverystartoftheprogram':
+        userSession.currentSession.resetSession()
+        userSession.currentSession.currentSubreddit = user_input_split[1]
+        userSession.currentSession.currentState = 'thread'
+    
+    elif userSession.currentSession.currentState == 'subreddit':
+        userSession.currentSession.currentState = 'thread'
+        userSession.currentSession.currentThread = 'hot'
+        if user_input_split[1] in valid_sorts:
+            userSession.currentSession.currentThread = user_input_split[1]
+        userSession.currentSession.resetThread()
+
+def cd_dot_command():
+    print 'cd . command'
+
+def cd_dot_dot_command():
+    # print 'cd .. command'
+    if userSession.currentSession.currentState == 'subreddit':
+        userSession.currentSession.currentSubreddit = ""
+        userSession.currentSession.currentState = 'attheverystartoftheprogram'
+    elif userSession.currentSession.currentState == 'thread':
+        userSession.currentSession.currentThread = ""
+        userSession.currentSession.currentState = 'subreddit'
+    elif userSession.currentSession.currentState == 'submission':
+        userSession.currentSession.currentSubmission = ""
+        userSession.currentSession.currentState = 'thread'
+    elif userSession.currentSession.currentState == 'comment':
+        userSession.currentSession.currentComment = ""
+        userSession.currentSession.currentState = 'submission'
+
+def ls_command():
+    if userSession.currentSession.currentState == 'thread':
+        redditGet.main_api_logic(userSession.currentSession.currentSubreddit, userSession.currentSession.currentThread, userSession.currentSession.nextNum)
+    elif userSession.currentSession.currentState == 'subreddit':
+        printThreads()
+    elif userSession.currentSession.currentState == 'submission':
+        redditGet.readComments(userSession.currentSession.currentSubmission, userSession.currentSession.nextNumComment)
+    elif userSession.currentSession.currentState == 'comment':
+        # TODO: Display subcomments
+        print "hi 2"
+
+def printThreads():
+    print "Please enter one of the following "
+    print valid_sorts
+
+def cat_command():
+    # TODO: cat
+    print 'cat command'
+
+def next_command():
+    if(userSession.currentSession.currentState == 'submission'):
+        userSession.currentSession.nextNumComment += 10
+        redditGet.readComments(userSession.currentSession.currentSubmission, userSession.currentSession.nextNumComment)
+    elif(userSession.currentSession.currentState == 'thread'):
+        userSession.currentSession.nextNum += 10
+        redditGet.main_api_logic(userSession.currentSession.currentSubreddit, userSession.currentSession.currentThread,
+                                 userSession.currentSession.nextNum)
+
+def previous_command():
+    if (userSession.currentSession.currentState == 'submission'):
+        userSession.currentSession.nextNumComment -= 10
+        if(userSession.currentSession.nextNumComment < 0):
+            userSession.currentSession.nextNumComment = 0
+        redditGet.readComments(userSession.currentSession.currentSubmission,
+                                       userSession.currentSession.nextNumComment)
+    elif (userSession.currentSession.currentState == 'thread'):
+        userSession.currentSession.nextNum -= 10
+        if(userSession.currentSession.nextNum < 0):
+            userSession.currentSession.nextNum = 0
+        redditGet.main_api_logic(userSession.currentSession.currentSubreddit,
+                                         userSession.currentSession.currentThread,
+                                         userSession.currentSession.nextNum)
+
+
+
+
+                # def next_command():
+#
+#     userSession.currentSession.nextNum += 10
+#     redditGet.readComments(userSession.currentSession.currentSubmission, userSession.currentSession.nextNum)
+
+    # if  (userSession.currentSession.currentState == 'submission'):
+    #     userSession.currentSession.nextNum += 10
+    #     redditGet.main_api_logic(userSession.currentSession.currentSubreddit, userSession.currentSession.currentThread, userSession.currentSession.nextNum)
+    # elif (userSession.currentSession.currentState == 'subreddit'):
+    #     userSession.currentSession.nextNum += 10
+    #     redditGet.readComments(userSession.currentSession.currentSubmission, userSession.currentSession.nextNum)
+
+def mkdir_command(title, body):
+    redditPost.createSubReddit(userSession.currentSession.currentSubreddit, title, body)
+
+def help_command():
+    helpInfo.printHelpInfo()
---
+++ redditGet.py
@@ -0,0 +1,77 @@
+import json
+import praw
+from praw.models import MoreComments
+import pprint
+
+# Custom files
+import userSession
+import commandFuncs
+import formatOutput
+
+# create an instance of reddit and subreddit using praw
+# Zak's credentials lolz
+reddit = praw.Reddit(client_id='ilgRMpq1J-Kx4w',
+                     client_secret='wsjtyoqyNVKm0Ds8nTTGJNU-YIE',
+                     user_agent='inter_webz')
+
+def storeItems(subreddit, subreddit_sort):
+    all_submissions_pre = "subreddit." + subreddit_sort + "(limit=100)"
+    all_submissions = eval(all_submissions_pre)
+
+    userSession.currentSession.itemsArr = []
+
+    for submission in all_submissions:
+        userSession.currentSession.itemsArr.append(submission)
+        userSession.currentSession.subId.append(submission.id)
+
+def main_api_logic(subReddit, subreddit_sort, nextNum):
+    this_subreddit = reddit.subreddit(userSession.currentSession.currentSubreddit)
+
+    if userSession.currentSession.nextNum == 0:
+        storeItems(this_subreddit, subreddit_sort)
+
+    itemsData = []
+    usernames = []
+    for item in userSession.currentSession.itemsArr:
+        itemsData.append(item.title)
+        try:
+            usernames.append(item.author.name)
+        except AttributeError:
+            usernames.append("Deleted user")
+    formatOutput.printList(usernames, itemsData, nextNum, ["37;40m", "35;40m"])
+
+def readPost(itemNum):
+    print formatOutput.displayTitle('Reading post: ', userSession.currentSession.itemsArr[itemNum-1].title)
+# <<<<<<< HEAD
+#     submission = reddit.submission(subId[itemNum-1])
+#     if (submission.url):
+#         print submission.url
+#     else:
+#         formatOutput.printPost(submission)
+# =======
+
+    submission = reddit.submission(userSession.currentSession.subId[itemNum-1])
+    if (submission.selftext):
+        formatOutput.printPost(submission)
+    else:
+        print submission.url
+
+    # formatOutput.printPost(submission)
+# >>>>>>> 0cec61483b3ca682423d9af593fd2dac91d05be1
+
+def readComments(itemNum, nextNum):
+    topComments = []
+    usernames = []
+    print formatOutput.displayTitle('Reading comments for: ', userSession.currentSession.itemsArr[itemNum-1].title)
+    submission = reddit.submission(userSession.currentSession.subId[itemNum - 1])
+    for top_level_comment in submission.comments:
+        if isinstance(top_level_comment, MoreComments):
+            continue
+        try:
+            usernames.append(top_level_comment.author.name)
+        except AttributeError:
+            usernames.append("Deleted user")
+        topComments.append(top_level_comment.body)
+
+    formatOutput.printList(usernames, topComments, nextNum, ["32;40m", "36;40m"])
+    
